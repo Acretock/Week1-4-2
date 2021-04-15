9a725414a31cb0be91065e25bc43137ed2d18705
Acretock <georgoncharov@gmail.com>
2021-04-15 14:25:26 +0500

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n, a, b, x, y;
+    cin >> n >> a >> b >> x >> y;
+    if (n > b)
+        cout << n * ((100.0 - y) / 100.0);
+    else if (n > a)
+        cout << n * ((100.0 - x) / 100.0);
+    else
+        cout << n;
+}
